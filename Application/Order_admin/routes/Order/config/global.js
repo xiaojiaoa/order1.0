a02aa65662bd67ac7f4ed7fcd139c0b3f66bd55b
806853602 <806853602@qq.com>
2017-02-13 16:03:02 +0800

    【UPDATE】增加.env 请重新运行npm install 把根目录的.env_example复制并改名.env

--- Application/Order_admin/routes/Order/config/global.js
+++ Application/Order_admin/routes/Order/config/global.js
@@ -4,32 +4,32 @@
 
 var Global = {
     site: {
-        author: '丫投科技',
+        author: process.env.AUTHOR || '丫投科技',
     },
     server: {
         //订单服务器
         Order: {
-            http: 'http://',
-            host: '192.2.17.63',
-            port: '8082',
+            http: process.env.SERVER_ORDER_HTTP || 'http://',
+            host: process.env.SERVER_ORDER_HOST || '192.2.17.63',
+            port: process.env.SERVER_ORDER_PORT || '8082',
             remote_server: function () {
                 return Global.server.Order.http + Global.server.Order.host + (Global.server.Order.port ? ':' + Global.server.Order.port : '');
             }
         },
         //文件上传服务器
         Upload: {
-            http: 'http://',
-            host: '192.2.17.21',
-            port: '8081',
+            http: process.env.SERVER_UPLOAD_HTTP || 'http://',
+            host: process.env.SERVER_UPLOAD_HOST || '192.2.17.21',
+            port: process.env.SERVER_UPLOAD_PORT || '8088',
             remote_server: function () {
                 return Global.server.Upload.http + Global.server.Upload.host + (Global.server.Upload.port ? ':' + Global.server.Upload.port : '');
             }
         },
         //静态资源服务器
         Static: {
-            http: 'http://',
-            host: '192.2.17.21',
-            port: '8088',
+            http: process.env.SERVER_STATIC_HTTP || 'http://',
+            host: process.env.SERVER_STATIC_HOST || '192.2.17.21',
+            port: process.env.SERVER_STATIC_PORT || '8088',
             remote_server: function () {
                 return Global.server.Static.http + Global.server.Static.host + (Global.server.Static.port ? ':' + Global.server.Static.port : '');
             }
