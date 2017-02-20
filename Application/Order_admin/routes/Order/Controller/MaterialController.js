d1693ea6382b8adafc2e7fe3f081580f7ddbefd2
xgr <1584576815@qq.com>
2017-02-20 11:57:29 +0800

    物料管理--物料分类

--- Application/Order_admin/routes/Order/Controller/MaterialController.js
+++ Application/Order_admin/routes/Order/Controller/MaterialController.js
@@ -29,11 +29,23 @@ var MaterialController = {
     materialTypePage: function (req, res) {
         res.render('order/material/material_type');
     },
-    materialTypeCreatePage: function (req, res) {
-        res.render('order/material/material_type_create');
+    materialTypeCreOnePage: function (req, res) {
+        res.render('order/material/material_type_creOne');
     },
-    materialTypeModifyPage: function (req, res) {
-        res.render('order/material/material_type_modify');
+    materialTypeCreTwoPage: function (req, res) {
+        res.render('order/material/material_type_creTwo');
+    },
+    materialTypeCreThreePage: function (req, res) {
+        res.render('order/material/material_type_creThree');
+    },
+    materialTypeChagOnePage: function (req, res) {
+        res.render('order/material/material_type_chagOne');
+    },
+    materialTypeChagTwoPage: function (req, res) {
+        res.render('order/material/material_type_chagTwo');
+    },
+    materialTypeChagThreePage: function (req, res) {
+        res.render('order/material/material_type_chagThree');
     },
     materialCreatePage: function (req, res) {
         res.render('order/material/material_create');
