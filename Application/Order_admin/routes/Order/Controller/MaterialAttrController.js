8cc73cf4d979e155f50636e5c8f2732d54802a1c
xgr <1584576815@qq.com>
2017-02-21 11:35:44 +0800

    物料属性--新建属性值接口

--- Application/Order_admin/routes/Order/Controller/MaterialAttrController.js
+++ Application/Order_admin/routes/Order/Controller/MaterialAttrController.js
@@ -16,46 +16,7 @@ var request = require('request');
 //引入权限
 var Permissions = require('../config/permission');
 
-var MaterialController = {
-    indexPage: function (req, res) {
-        res.render('order/material/material_index');
-    },
-    detailPage: function (req, res) {
-    res.render('order/material/material_detail');
-    },
-    summaryPage: function (req, res) {
-        res.render('order/material/material_summary');
-    },
-    materialTypePage: function (req, res) {
-        res.render('order/material/material_type');
-    },
-    materialTypeCreOnePage: function (req, res) {
-        res.render('order/material/material_type_creOne');
-    },
-    materialTypeCreTwoPage: function (req, res) {
-        res.render('order/material/material_type_creTwo');
-    },
-    materialTypeCreThreePage: function (req, res) {
-        res.render('order/material/material_type_creThree');
-    },
-    materialTypeChagOnePage: function (req, res) {
-        res.render('order/material/material_type_chagOne');
-    },
-    materialTypeChagTwoPage: function (req, res) {
-        res.render('order/material/material_type_chagTwo');
-    },
-    materialTypeChagThreePage: function (req, res) {
-        res.render('order/material/material_type_chagThree');
-    },
-    materialCreatePage: function (req, res) {
-        res.render('order/material/material_create');
-    },
-    materialModifyPage: function (req, res) {
-        res.render('order/material/material_modify');
-    },
-    doCreate: function (req, res) {
-        console.log('物料创建'+ JSON.stringify(req.body));
-    },
+var MaterialAttrController = {
     materialAttributePage:function (req, res) {
         var paramObject = helper.genPaginationQuery(req);
         Base.multiDataRequest(req, res, [
@@ -75,7 +36,7 @@ var MaterialController = {
                 title: ' ',
                 pagination: boostrapPaginator.render()
             },resultList));
-        res.render('order/material/material_attribute',returnData);
+            res.render('order/material/material_attribute',returnData);
         });
     },
     attrCreate: function (req, res) {
@@ -93,11 +54,11 @@ var MaterialController = {
         })
     },
     attrChange: function (req, res) {
-        //console.log('修改物料属性'+ JSON.stringify(req.body));
-        var mid = req.body.storeId;
+       // console.log('修改物料属性'+ JSON.stringify(req.body));
+        var aid = req.body.aid;
         request(Base.mergeRequestOptions({
             method: 'put',
-            url: '/api/stores/' + cid + "?" + queryString.stringify(req.body),
+            url: '/api/attributes/' + aid + "?" + queryString.stringify(req.body),
         }, req, res), function (error, response, body) {
             if (!error && response.statusCode == 201) {
                 res.redirect("/materialManage/materialAttribute");
@@ -122,21 +83,8 @@ var MaterialController = {
         })
 
     },
-    mateAttrDetailPage: function (req, res) {
-       /*  var mid =  req.params.mid;
-         Base.multiDataRequest(req, res, [
-         {url: '/api/attributes/'+mid, method: 'GET', resConfig: {keyName: 'attributeDetail', is_must: true}},
-         ], function (req, res, resultList) {
-
-         var returnData = Base.mergeData(helper.mergeObject({
-         title: ' ',
-         },resultList));
-             res.render('order/material/material_attribute_detail',returnData);
-         });*/
-        res.render('order/material/material_attribute_detail');
-    },
     attrValCreate: function (req, res) {
-        //console.log('增加物料的属性值'+ JSON.stringify(req.body));
+        console.log('增加物料的属性值'+ JSON.stringify(req.body));
         request(Base.mergeRequestOptions({
             method: 'post',
             url: '/api/attributes/value',
@@ -149,7 +97,20 @@ var MaterialController = {
             }
         })
     },
+    mateAttrDetailPage: function (req, res) {
+        /*  var mid =  req.params.mid;
+         Base.multiDataRequest(req, res, [
+         {url: '/api/attributes/'+mid, method: 'GET', resConfig: {keyName: 'attributeDetail', is_must: true}},
+         ], function (req, res, resultList) {
+
+         var returnData = Base.mergeData(helper.mergeObject({
+         title: ' ',
+         },resultList));
+         res.render('order/material/material_attribute_detail',returnData);
+         });*/
+        res.render('order/material/material_attribute_detail');
+    },
 };
 
-module.exports = MaterialController;
+module.exports = MaterialAttrController;
 
