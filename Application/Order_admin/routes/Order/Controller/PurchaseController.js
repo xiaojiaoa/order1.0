b39ac6e12c4722244b18b4b0003352bc985bd833
1651885856 <1651885856@qq.com>
2017-02-21 09:22:24 +0800

    供应商+采购

---
+++ Application/Order_admin/routes/Order/Controller/PurchaseController.js
@@ -0,0 +1,44 @@
+//模板 供复制用
+var Base = require('./BaseController');
+
+//分页
+var Pagination = require('pagination');
+
+//生成请求query
+var queryString = require('qs');
+
+//自定义帮助函数
+var helper = require('../config/helper');
+
+//请求模块
+var request = require('request');
+
+//引入权限
+var Permissions = require('../config/permission');
+
+
+var supplierController = {
+    supplierPage: function (req, res) {
+        res.render('order/supplier/index');
+    },
+    supplierSortPage: function (req, res) {
+        res.render('order/supplier/sort');
+    },
+    supplierCreatPage: function (req, res) {
+        res.render('order/supplier/creat');
+    },
+    supplierModifyPage: function (req, res) {
+        res.render('order/supplier/modify');
+    },
+    supplierSortCreatPage: function (req, res) {
+        res.render('order/supplier/sort_creat');
+    },
+    supplierOfferProductPage: function (req, res) {
+        res.render('order/supplier/offer_product');
+    },
+    supplierSortModifyPage: function (req, res) {
+        res.render('order/supplier/sort_modify');
+    },
+};
+
+module.exports = supplierController;
