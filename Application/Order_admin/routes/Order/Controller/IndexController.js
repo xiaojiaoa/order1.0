68b6941fbd34546982c868e08d0a4631e76ddc8a
xgr <1584576815@qq.com>
2017-03-29 11:35:15 +0800

    客户统计静态页面

--- Application/Order_admin/routes/Order/Controller/IndexController.js
+++ Application/Order_admin/routes/Order/Controller/IndexController.js
@@ -5,13 +5,45 @@ var helper = require('../config/helper');
 
 var request = require('request');
 
+var Pagination = require('pagination');
+
+var queryString = require('qs');
+
 
 var IndexController = {
 
     indexPage: function (req, res) {
         res.redirect('/userCenter')
         // res.render('order/index', Base.mergeData(helper.mergeObject({title: '订单登录系统'}, {})));
-    }
+    },
+    countCustomerPage: function (req, res) {
+        Base.multiDataRequest(req, res, [
+                {url: '/api/taskseqs/custcount?'+ queryString.stringify(req.query), method: 'GET', resConfig: {keyName: 'custcountInfo', is_must: true}},
+                {url: '/api/stores/list', method: 'GET', resConfig: {keyName: 'storesList', is_must: true}},
+                {url: '/api/stores/employees/homeAdviser', method: 'GET', resConfig: {keyName: 'homeAdviserInfo', is_must: true}},
+            ],
+            function (req, res, resultList) {
+                var returnData = Base.mergeData(helper.mergeObject({
+                    title: ' ',
+                }, resultList));
+
+                res.render('order/count/count_customer', returnData);
+            });
+    },
+    countDealPage: function (req, res) {
+        Base.multiDataRequest(req, res, [
+                {url: '/api/taskseqs/custcount/deal?'+ queryString.stringify(req.query), method: 'GET', resConfig: {keyName: 'dealInfo', is_must: true}},
+                {url: '/api/stores/designers', method: 'GET', resConfig: {keyName: 'designerInfo', is_must: true}},
+                {url: '/api/stores/employees/homeAdviser', method: 'GET', resConfig: {keyName: 'homeAdviserInfo', is_must: true}},
+            ],
+            function (req, res, resultList) {
+                var returnData = Base.mergeData(helper.mergeObject({
+                    title: ' ',
+                }, resultList));
+
+                res.render('order/count/count_deal', returnData);
+            });
+    },
 };
 
 module.exports = IndexController;
--- Application/Order_admin/routes/Order/home.js
+++ Application/Order_admin/routes/Order/home.js
@@ -146,6 +146,14 @@ var IndexController = require('./Controller/IndexController');
 // 首页页面
 router.get('/', Middleware.AuthCheck, IndexController.indexPage);
 
+// 客户跟进统计
+router.get('/countCustomer', Middleware.AuthCheck, IndexController.countCustomerPage);
+
+// 成交情况统计
+router.get('/countDeal', Middleware.AuthCheck, IndexController.countDealPage);
+
+
+
 /*
  * 页面范围: 用户操作相关
  * 控制器:   UserController
