56b0bd45ef5a4112bb9d06874105c5a29599e01c
1651885856 <1651885856@qq.com>
2017-06-14 17:47:41 +0800

    财务统计 初始化 条件

--- Application/Order_admin/routes/Order/Controller/FinancialStatisticsController.js
+++ Application/Order_admin/routes/Order/Controller/FinancialStatisticsController.js
@@ -18,13 +18,27 @@ var Permissions = require('../config/permission');
 
 
 var FinancialStatisticsController = {
+
     listPage:function(req,res){
+        // console.log('payTimeStart',JSON.stringify(req.query))
+        // var ifInitTime = req.query.ifInitTime;
+        if(!req.query.ifInitTime){
+            var date_ = new Date();
+            var year = date_.getFullYear();
+            var month = date_.getMonth() + 1;
+            var firstdate = year + '-' + month + '-01';
+            var day = new Date(year,month,0);
+            var lastdate = year + '-' + month + '-' + day.getDate();
+            return res.redirect('/FinancialStatistics/index?payTimeStart='+firstdate+'&payTimeEnd='+lastdate+'&ifInitTime=1');
+        }
+
         Base.multiDataRequest(req, res, [
                 {url: '/api/orders/financial/statistics?'+ queryString.stringify(req.query), method: 'GET', resConfig: {keyName: 'FinancialStatisticsList', is_must: true}},
                 {url: '/api/assist/order/stcodes', method: 'GET', resConfig: {keyName: 'stcodeInfo', is_must: false}},
                 {url: '/api/assist/deco/color', method: 'GET', resConfig: {keyName: 'colorList', is_must: true}},
             ],
             function (req, res, resultList) {
+
                 var returnData = Base.mergeData(helper.mergeObject({
                     title: ' ',
                 }, resultList));
