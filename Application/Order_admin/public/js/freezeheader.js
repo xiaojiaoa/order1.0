9537413963487579f7ebf245bd1f1931353f4b37
jxy <jxy@qq.com>
2017-06-15 17:40:45 +0800

    表头固定的表格实现方法调整，现在的方法表格内容自适应排版（原来的表格内容每列平分），待测试兼容性后再做全局替换

--- Application/Order_admin/public/js/freezeheader.js
+++ Application/Order_admin/public/js/freezeheader.js
@@ -13,97 +13,50 @@
  Version: 1.0.7
  -------------------------------------------------------------------------*/
 (function ($) {
-  var TABLE_ID = 0;
-  $.fn.freezeHeader = function (params) {
+    var TABLE_ID = 0;
+    $.fn.freezeHeader = function (params) {
 
-    // var copiedHeader = false;
+        // var copiedHeader = false;
 
-    function freezeHeader(elem) {
-      var idObj = elem.attr('id') || ('tbl-' + (++TABLE_ID));
-      if (elem.length > 0 && elem[0].tagName.toLowerCase() == "table") {
+        function freezeHeader(elem) {
+            var idObj = elem.attr('id') || ('tbl-' + (++TABLE_ID));
+            if (elem.length > 0 && elem[0].tagName.toLowerCase() == "table") {
 
-        var obj = {
-          id: idObj,
-          grid: elem,
-          container: null,
-          header: null,
-          divScroll: null,
-          openDivScroll: null,
-          closeDivScroll: null,
-          scroller: null
-        };
+                var obj = {
+                    id: idObj,
+                    grid: elem,
+                    header: elem.find('thead'),
+                };
 
 
-          obj.header = obj.grid.find('thead');
-          if (params && params.height !== undefined) {
-              if ($('#hdScroll' + obj.id).length == 0) {
-                  obj.grid.wrapAll(obj.divScroll);
-              }
-          }
+                // console.log( 'header',obj.header)
 
 
+                // var firstTd = obj.grid.find('tbody tr').eq(0);
+
+                // console.log( 'firstTd',firstTd)
+                // console.log( 'firstTd',firstTd.width())
+                // console.log( 'header',obj.header)
 
 
+                elem.append('<tfoot>'+obj.header.html()+'</tfoot>')
+                var tfoot = elem.find('tfoot')
+                tfoot.find('th').each(function (index) {
+                    var cellWidth = obj.header.find('th').eq(index).css('width');
+                    $(this).css('width', cellWidth);
+                    tfoot.css('position','absolute').css('top',0);
+                });
+                // elem.append(footer)
 
-        if (params && params.height !== undefined) {
-          obj.divScroll = '<div id="hdScroll' + obj.id + '" style="height: ' + params.height + '; overflow-y: scroll">';
-          obj.closeDivScroll = '</div>';
+
+            }
         }
 
 
-
-        obj.scroller = params && params.height !== undefined
-          ? $('#hdScroll' + obj.id)
-          : $(window);
-        if (params && params.scrollListenerEl !== undefined) {
-          obj.scroller = params.scrollListenerEl;
-        }
-        obj.scroller.on('scroll', function () {
-          if ($('#hd' + obj.id).length == 0) {
-            obj.grid.before('<div id="hd' + obj.id + '"></div>');
-          }
-
-          obj.container = $('#hd' + obj.id);
-
-            cloneHeaderRow(obj);
-            // copiedHeader = true;
-
+        return this.each(function (i, e) {
+            freezeHeader($(e));
+            TABLE_ID ++;
         });
-      }
-    }
 
-    function cloneHeaderRow(obj) {
-      obj.container.html('');
-      obj.container.val('');
-      var tabela = $('<table style="margin: 0 0;"></table>');
-      var atributos = obj.grid.prop("attributes");
-
-      $.each(atributos, function () {
-        if (this.name != "id") {
-          tabela.attr(this.name, this.value);
-        }
-      });
-
-      tabela.append('<thead>' + obj.header.html() + '</thead>');
-
-      obj.container.append(tabela);
-      obj.container.width(obj.header.width());
-      obj.container.height(obj.header.height);
-      obj.container.find('th').each(function (index) {
-        var cellWidth = obj.grid.find('th').eq(index).width();
-        $(this).css('width', cellWidth);
-      });
-
-      obj.container.css("visibility", "visible");
-      obj.container.css("z-index", "999");
-      obj.container.css("top", "0px");
-      obj.container.css("position", "absolute");
-
-    }
-
-    return this.each(function (i, e) {
-      freezeHeader($(e));
-    });
-
-  };
+    };
 })(jQuery);
