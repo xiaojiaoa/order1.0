31045497fc158b71a1e1870f290e69516389df47
jxy <jxy@qq.com>
2017-08-14 10:19:30 +0800

    订单审核-审核总价-价格比例每个门店不同

--- Application/Order_admin/routes/Order/Controller/OrderController.js
+++ Application/Order_admin/routes/Order/Controller/OrderController.js
@@ -809,7 +809,7 @@ var OrderController = {
                 {url: '/api/assist/review/apartReason', method: 'GET', resConfig: {keyName: 'apartReason', is_must: true}},
                 {url: '/api/assist/review/apartReviewReason', method: 'GET', resConfig: {keyName: 'apartReviewReason', is_must: true}},
                 {url: '/api/assist/review/scheduleReason', method: 'GET', resConfig: {keyName: 'scheduleReason', is_must: true}},
-                {url: '/api/cofficient', method: 'GET', resConfig: {keyName: 'cofficientInfo', is_must: true}},
+                {url: '/api/cofficient/'+tid, method: 'GET', resConfig: {keyName: 'cofficientInfo', is_must: true}},
 
                 // {url: '/api/assist/orderfile/type', method: 'GET', resConfig: {keyName: 'allFileTypeInfo', is_must: true}},
                 {url: '/api/orders/chgback/'+tid, method: 'GET', resConfig: {keyName: 'chgbackInfo', is_must: true}},
